d453ea62b2b244473290140c8fd1c7bb18371fc6
Stefan Boos <stefan.boos@gmx.de>
2021-02-26 06:55:31 +0100

    implement all except one test cases

--- csharp.Lib.Tests/BalancedStringValidatorTest.cs
+++ csharp.Lib.Tests/BalancedStringValidatorTest.cs
@@ -16,17 +16,19 @@ namespace csharp.Lib.Tests
         [InlineData(false, "Sensei says no!)", "()")]
         [InlineData(true, "[Sensei says yes!]", "[]")]
         [InlineData(false, "[Sensei says no!", "[]")]
-        public void IsBalanced_SingleParentheses_ReturnsExpectedResult(bool expectation, string input, string balancingCharacterPairs)
+        public void IsBalanced_SingleParentheses_ReturnsExpectedResult(bool expectation, string input,
+            string balancingCharacterPairs)
         {
             Assert.Equal(expectation, input.IsBalanced(balancingCharacterPairs));
         }
 
-        [Theory(Skip = "Not implemented yet")]
+        [Theory]
         [InlineData(true, "(Sensei [says] yes!)", "()[]")]
         [InlineData(false, "(Sensei [says) no!]", "()[]")]
         [InlineData(true, "Sensei says 'yes'!", "''")]
         [InlineData(false, "Sensei say's no!", "''")]
-        public void IsBalanced_DataFromCodeWars_ExpectedResultFromCodewars(bool expectation, string input, string balancingCharacterPairs)
+        public void IsBalanced_DataFromCodeWars_ExpectedResultFromCodewars(bool expectation, string input,
+            string balancingCharacterPairs)
         {
             Assert.Equal(expectation, input.IsBalanced(balancingCharacterPairs));
         }
--- csharp.Lib/BalancedStringValidator.cs
+++ csharp.Lib/BalancedStringValidator.cs
@@ -8,24 +8,70 @@ namespace csharp.Lib
     {
         public static bool IsBalanced(this string input, string balancingCharacterPairs)
         {
-            var usedOpeningCharacters = new Stack<char>();
-            var validOpeningCharacters = balancingCharacterPairs.Where((c, index) => index % 2 == 0);
-
-            var isParenOpened = false;
+            var reqClosingCharacters = new ClosingCharacters(balancingCharacterPairs);
             foreach (var character in input)
             {
-                switch (character)
+                if (reqClosingCharacters.IsOpeningCharacter(character))
                 {
-                    case '(':
-                    case '[':
-                        isParenOpened = true;
-                        break;
-                    case ')':
-                    case ']':
-                        return isParenOpened;
+                    reqClosingCharacters.AddIfOpeningCharacter(character);
+                }
+                else if (reqClosingCharacters.IsClosingCharacter(character))
+                {
+                    if (!reqClosingCharacters.CheckAndPopClosingCharacter(character))
+                    {
+                        return false;
+                    }
                 }
             }
-            return !isParenOpened;
+
+            return reqClosingCharacters.IsEmpty();
         }
+    }
+
+    public class ClosingCharacters
+    {
+        private readonly Dictionary<char, char> _mapOpeningToClosingCharacters;
+        private readonly Stack<char> _requiredClosingCharacters = new();
+
+        public ClosingCharacters(string balancingCharacterPairs) =>
+            _mapOpeningToClosingCharacters = MapOpeningToClosingCharacters(balancingCharacterPairs);
+
+        private static Dictionary<char, char> MapOpeningToClosingCharacters(string balancingCharacterPairs)
+        {
+            return balancingCharacterPairs.Zip(
+                    balancingCharacterPairs.Skip(1),
+                    Tuple.Create)
+                .Where((_, index) => index % 2 == 0)
+                .ToDictionary(tuple => tuple.Item1, tuple => tuple.Item2);
+        }
+
+        public void AddIfOpeningCharacter(char character)
+        {
+            if (IsOpeningCharacter(character))
+            {
+                _requiredClosingCharacters.Push(_mapOpeningToClosingCharacters[character]);
+            }
+        }
+
+        public bool CheckAndPopClosingCharacter(char character)
+        {
+            if (_requiredClosingCharacters.Count == 0)
+            {
+                return false;
+            }
+
+            if (_requiredClosingCharacters.Pop() != character)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool IsClosingCharacter(char character) => _mapOpeningToClosingCharacters.Values.Contains(character);
+
+        public bool IsOpeningCharacter(char character) => _mapOpeningToClosingCharacters.Keys.Contains(character);
+
+        public bool IsEmpty() => _requiredClosingCharacters.Count == 0;
     }
 }
